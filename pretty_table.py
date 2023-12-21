0f29ac45458abb759d61b43de5a5fb8706dabc46
Afzal Nomani <afzalnomani1@gmail.com>
2023-12-21 13:11:36 +0530

    add total marks to table

--- pretty_table.py
+++ pretty_table.py
@@ -4,5 +4,6 @@ table = PrettyTable()
 
 table.add_column('Subjects',['Mathematics',"Physics","Chemistry"])
 table.add_column('Marks Obtained',[90,88,77])
+table.add_column('Total Marks',[100,100,100])
 
 print(table)
